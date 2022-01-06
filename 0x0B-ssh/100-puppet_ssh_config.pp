76e32cbf1ba7edd2334407215bb46d2462a906ba
juandraper <e146613@gmail.com>
2022-01-05 16:39:30 -0800

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,15 @@
+#Changes the configuration of the ssh client.
+
+file_line {'/etc/ssh/ssh_config':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  match  => '^#\s{3,}PasswordAuthentication',
+}
+
+file_line {'/etc/ssh/ssh_config22':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => '^#\s{3,}IdentityFile ~/.ssh/id_ed25519',
+}
